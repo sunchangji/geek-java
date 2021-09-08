f436c1368d804fdaa7556bda036e4595d6b99b46
指尖舞步 <595496850@qq.com>
2021-09-08 12:41:36 +0800

    搭建 ActiveMQ 服务，基于 JMS，写代码分别实现对于 queue 和 topic 的消息生产和消费，代码提交到 github

---
+++ week12/activemq/src/test/java/com/suncj/activemq/ActivemqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suncj.activemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivemqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
